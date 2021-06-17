3db86e608cd26a81a7e2d05a329459c0cab45b87
matthewgagne1 <84517315+matthew-gagne@users.noreply.github.com>
2021-06-17 02:26:33 +0000

    more and more fixes

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
 let d = 0
+let C = 0
+let A= 0
+let R = 0
 document.getElementById('button').addEventListener('click', calculate)
 
 function calculate () {
   d = document.getElementById('d').value
 
-    if (d > 0) {
-  var C;
-  var A;
-  var R = d/2;
+  if (d > 0) {
+  R = d/2;
   C = 2 * Math.PI * (d / 2)
   A = Math.PI * Math.pow(R,2)
-  alert('the area is '+ A + ' and the circumference is ' + C)
+  alert('the area is ' + A + ' and the circumference is ' + C)
   }else{
     alert('input must be greater than ZERO')
   }
