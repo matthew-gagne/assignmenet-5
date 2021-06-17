9b3c72b61076e7982d3ec73e23636240b68daef5
matthewgagne1 <84517315+matthew-gagne@users.noreply.github.com>
2021-06-17 02:08:04 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+document.getElementById('button').addEventListener('click', calculate);
+
+function calculate () {
+  var d = document.getElementById('d').value;
+  if (d > 0){
+
+  
+  var C;
+  var A;
+  var R = d/2;
+  C = 2 * Math.PI * (d / 2);
+  A = Math.PI * Math.pow(R,2);
+  alert('the area is '+ A + ' and the circumference is ' + C);
+  }else{
+    alert('input must be greater than ZERO');
+  }
+}
