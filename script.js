9f8555572172806637925b4af4581b431033a5ff
matthewgagne1 <84517315+matthew-gagne@users.noreply.github.com>
2021-06-17 02:31:07 +0000

    mor fix

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let d = 0
 let C = 0
-let A= 0
+let A = 0
 let R = 0
 document.getElementById('button').addEventListener('click', calculate)
 
@@ -8,11 +8,11 @@ function calculate () {
   d = document.getElementById('d').value
 
   if (d > 0) {
-  R = d/2;
-  C = 2 * Math.PI * (d / 2)
-  A = Math.PI * Math.pow(R,2)
-  alert('the area is ' + A + ' and the circumference is ' + C)
-  }else{
+    R = d / 2
+    C = 2 * Math.PI * (d / 2)
+    A = Math.PI * Math.pow(R, 2)
+    alert('the area is ' + A + ' and the circumference is ' + C)
+  } else {
     alert('input must be greater than ZERO')
   }
 }
